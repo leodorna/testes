de93e846babecb6cfaa0b399b243eb308a2ba7c9
leodorna <leonardo.dorneles@inf.ufrgs.br>
2018-09-26 13:21:40 -0300

    [ADD] Ionic Component, Interfaces and Providers for Donut Visualization using d3

---
+++ resources/ionic/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { DonutComponent } from './donut/donut';
+@NgModule({
+	declarations: [DonutComponent],
+	imports: [],
+	exports: [DonutComponent]
+})
+export class ComponentsModule {}
